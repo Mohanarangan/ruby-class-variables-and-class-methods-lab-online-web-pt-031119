0672e849708942de7caafcd92a7d5769d6a84a87
chandra mohan <mohaniyer_78@hotmail.com>
2019-04-07 22:26:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,7 @@ class Song
     genre_count={}
     @@genres.each do |genre|
      #binding.pry
-      if genre_count[genre]	
+    
       if genre_count.has_key?(genre)
         genre_count[genre]+=1
       else
